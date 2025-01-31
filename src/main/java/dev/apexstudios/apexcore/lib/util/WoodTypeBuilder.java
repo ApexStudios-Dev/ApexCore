0d438d4257ca258b0e36078ed39b3346896b60d7
Apex <robsonlawson3@gmail.com>
2025-01-31 04:05:51 +0000

    Add block set/wood type builders

---
+++ src/main/java/dev/apexstudios/apexcore/lib/util/WoodTypeBuilder.java
@@ -0,0 +1,71 @@
+package dev.apexstudios.apexcore.lib.util;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.properties.WoodType;
+
+public final class WoodTypeBuilder {
+    private final BlockSetTypeBuilder blockSetType = BlockSetTypeBuilder.builder();
+    private SoundType soundType = SoundType.WOOD;
+    private SoundType hangingSignSoundType = SoundType.HANGING_SIGN;
+    private Supplier<SoundEvent> fenceGateClose = () -> SoundEvents.FENCE_GATE_CLOSE;
+    private Supplier<SoundEvent> fenceGateOpen = () -> SoundEvents.FENCE_GATE_OPEN;
+
+    private WoodTypeBuilder() {
+    }
+
+    public WoodTypeBuilder soundType(SoundType soundType) {
+        this.soundType = soundType;
+        return this;
+    }
+
+    public WoodTypeBuilder hangingSignSoundType(SoundType hangingSignSoundType) {
+        this.hangingSignSoundType = hangingSignSoundType;
+        return this;
+    }
+
+    public WoodTypeBuilder fenceGateClose(Supplier<SoundEvent> fenceGateClose) {
+        this.fenceGateClose = fenceGateClose;
+        return this;
+    }
+
+    public WoodTypeBuilder fenceGateOpen(Supplier<SoundEvent> fenceGateOpen) {
+        this.fenceGateOpen = fenceGateOpen;
+        return this;
+    }
+
+    public WoodTypeBuilder blockSetType(Consumer<BlockSetTypeBuilder> blockSetTypeBuilder) {
+        blockSetTypeBuilder.accept(blockSetType);
+        return this;
+    }
+
+    public WoodTypeBuilder copy(WoodType woodType) {
+        return soundType(woodType.soundType())
+                .hangingSignSoundType(woodType.hangingSignSoundType())
+                .fenceGateClose(woodType::fenceGateClose)
+                .fenceGateOpen(woodType::fenceGateOpen)
+                .blockSetType(builder -> builder.copy(woodType.setType()));
+    }
+
+    public WoodType build(String woodTypeName, String blockSetTypeName) {
+        return WoodType.register(new WoodType(
+                woodTypeName,
+                blockSetType.build(blockSetTypeName),
+                soundType,
+                hangingSignSoundType,
+                fenceGateClose.get(),
+                fenceGateOpen.get()
+        ));
+    }
+
+    public WoodType build(String name) {
+        return build(name, name);
+    }
+
+    public static WoodTypeBuilder builder() {
+        return new WoodTypeBuilder();
+    }
+}
