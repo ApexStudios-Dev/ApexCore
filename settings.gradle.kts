de6e0a663715e55ee0f3421134468f069ccd35e9
Apex <robsonlawson3@gmail.com>
2025-02-15 16:13:12 +0000

    Tear out github package publishing
    Use snapshot versioning for ApexGradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,24 +8,12 @@ pluginManagement {
         mavenLocal()
 
         maven("https://maven.apexstudios.dev/releases")
-
-        val GITHUB_ACTOR = providers.gradleProperty("gpr.user").orElse(providers.environmentVariable("GITHUB_ACTOR"))
-        val GITHUB_TOKEN = providers.gradleProperty("gpr.token").orElse(providers.environmentVariable("GITHUB_TOKEN"))
-
-        if(GITHUB_ACTOR.isPresent && GITHUB_TOKEN.isPresent) {
-            maven("https://maven.pkg.github.com/ApexStudios-Dev/ApexGradle") {
-                credentials {
-                    username = GITHUB_ACTOR.get()
-                    password = GITHUB_TOKEN.get()
-                }
-            }
-        }
     }
 
     resolutionStrategy {
         eachPlugin {
             if(requested.id.namespace == "apex-conventions") {
-                useVersion("[0.1,)")
+                useVersion("0.1-SNAPSHOT")
             }
         }
     }
