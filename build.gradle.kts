9bf08f4a404bb443fc966595c08c99b36da69753
Apex <robsonlawson3@gmail.com>
2025-03-25 16:46:49 +0000

    Prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,24 +8,17 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.5.0-alpha.1.21.5-rc1.20250325.145610", "1.21.4", "2025.03.23")
+apex.neoVersion("21.5.0-beta", "1.21.4", "2025.03.23")
 apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
 
 neoForge {
-    validateAccessTransformers.set(false)
-
     accessTransformers {
         from(file("src/${SourceSet.MAIN_SOURCE_SET_NAME}/resources/META-INF/accesstransformer-dev.cfg"))
         publish(file("src/${SourceSet.MAIN_SOURCE_SET_NAME}/resources/META-INF/accesstransformer-dev.cfg"))
     }
-}
-
-repositories {
-    maven("https://maven.apexstudios.dev/private")
-    apex.neoPrMaven(this, 2039)
 }
 
 dependencies {
