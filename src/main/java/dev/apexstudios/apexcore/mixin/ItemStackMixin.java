eefd9cecbba7e211983da81bd2f15e75d751fb8a
Apex <29412632+ApexModder@users.noreply.github.com>
2025-04-16 09:33:24 +0100

    [21.5] Fix tooltip mixins not being fully applied (#27)
    [no ci]

--- src/main/java/dev/apexstudios/apexcore/mixin/ItemStackMixin.java
+++ src/main/java/dev/apexstudios/apexcore/mixin/ItemStackMixin.java
@@ -3,98 +3,101 @@ package dev.apexstudios.apexcore.mixin;
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.apexstudios.apexcore.core.util.TooltipMutationHandler;
 import dev.apexstudios.apexcore.lib.tooltip.TooltipOrder;
 import dev.apexstudios.apexcore.lib.tooltip.TooltipPosition;
 import java.util.List;
 import java.util.function.Consumer;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-    @Inject(
+    @WrapOperation(
             method = "getTooltipLines",
             at = @At(
                     value = "INVOKE",
-                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
-                    ordinal = 0,
-                    shift = At.Shift.BEFORE
+                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z"
             )
     )
-    private void ApexCore$appendTooltipHeaderBefore(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> consumer) {
-        TooltipMutationHandler.mutate(TooltipPosition.HEADER, TooltipOrder.BEFORE).accept((ItemStack) (Object) this, context, consumer::add, player, flag);
-    }
-
-    @Inject(
-            method = "getTooltipLines",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
-                    ordinal = 0,
-                    shift = At.Shift.AFTER
-            )
-    )
-    private void ApexCore$appendTooltipHeaderAfter(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> consumer) {
-        TooltipMutationHandler.mutate(TooltipPosition.HEADER, TooltipOrder.AFTER).accept((ItemStack) (Object) this, context, consumer::add, player, flag);
-    }
-
-    @Inject(
-            method = "getTooltipLines",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/item/ItemStack;has(Lnet/minecraft/core/component/DataComponentType;)Z",
-                    ordinal = 2,
-                    shift = At.Shift.BEFORE
-            )
-    )
-    private void ApexCore$appendTooltipItemBefore(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local Consumer<Component> consumer) {
-        TooltipMutationHandler.mutate(TooltipPosition.ITEM, TooltipOrder.BEFORE).accept((ItemStack) (Object) this, context, consumer, player, flag);
-    }
-
-    @Inject(
-            method = "getTooltipLines",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/item/ItemStack;addToTooltip(Lnet/minecraft/core/component/DataComponentType;Lnet/minecraft/world/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/world/item/TooltipFlag;)V",
-                    ordinal = 0,
-                    shift = At.Shift.BEFORE
-            )
-    )
-    private void ApexCore$appendTooltipComponentBefore(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local Consumer<Component> consumer) {
+    private boolean ApexCore$appendTooltipHeaderBefore(
+            List<Component> lines,
+            Object header,
+            Operation<Boolean> original,
+            @Local(argsOnly = true) Item.TooltipContext context,
+            @Local(argsOnly = true) @Nullable Player player,
+            @Local(argsOnly = true) TooltipFlag flag
+    ) {
         var self = (ItemStack) (Object) this;
-        TooltipMutationHandler.mutate(TooltipPosition.ITEM, TooltipOrder.AFTER).accept(self, context, consumer, player, flag);
-        TooltipMutationHandler.mutate(TooltipPosition.COMPONENT, TooltipOrder.BEFORE).accept(self, context, consumer, player, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.HEADER, TooltipOrder.BEFORE).accept(self, context, lines::add, player, flag);
+        var result = original.call(lines, header);
+        TooltipMutationHandler.mutate(TooltipPosition.HEADER, TooltipOrder.AFTER).accept(self, context, lines::add, player, flag);
+        return result;
     }
 
-    @Inject(
-            method = "getTooltipLines",
+    @WrapOperation(
+            method = "addDetailsToTooltip",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/item/ItemStack;addToTooltip(Lnet/minecraft/core/component/DataComponentType;Lnet/minecraft/world/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/world/item/TooltipFlag;)V",
-                    ordinal = 8,
-                    shift = At.Shift.AFTER
+                    target = "Lnet/minecraft/world/item/Item;appendHoverText(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/Item$TooltipContext;Lnet/minecraft/world/item/component/TooltipDisplay;Ljava/util/function/Consumer;Lnet/minecraft/world/item/TooltipFlag;)V"
             )
     )
-    private void ApexCore$appendTooltipComponentAfter(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local Consumer<Component> consumer) {
-        TooltipMutationHandler.mutate(TooltipPosition.COMPONENT, TooltipOrder.AFTER).accept((ItemStack) (Object) this, context, consumer, player, flag);
+    private void ApexCore$appendTooltipItemBefore(
+            Item item,
+            ItemStack stack,
+            Item.TooltipContext context,
+            TooltipDisplay display,
+            Consumer<Component> adder,
+            TooltipFlag flag,
+            Operation<Void> original,
+            @Local(argsOnly = true) @Nullable Player player
+    ) {
+        TooltipMutationHandler.mutate(TooltipPosition.ITEM, TooltipOrder.BEFORE).accept(stack, context, adder, player, flag);
+        original.call(item, stack, context, display, adder, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.ITEM, TooltipOrder.AFTER).accept(stack, context, adder, player, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.COMPONENT, TooltipOrder.BEFORE).accept(stack, context, adder, player, flag);
+    }
+
+    @WrapOperation(
+            method = "addDetailsToTooltip",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;addToTooltip(Lnet/minecraft/core/component/DataComponentType;Lnet/minecraft/world/item/Item$TooltipContext;Lnet/minecraft/world/item/component/TooltipDisplay;Ljava/util/function/Consumer;Lnet/minecraft/world/item/TooltipFlag;)V",
+                    ordinal = 21
+            )
+    )
+    private void ApexCore$appendTooltipComponentAfter(
+            ItemStack stack,
+            DataComponentType<?> componentType,
+            Item.TooltipContext context,
+            TooltipDisplay display,
+            Consumer<Component> adder,
+            TooltipFlag flag,
+            Operation<Void> original,
+            @Local(argsOnly = true) @Nullable Player player
+    ) {
+        original.call(stack, componentType, context, display, adder, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.COMPONENT, TooltipOrder.AFTER).accept(stack, context, adder, player, flag);
     }
 
     @Definition(id = "player", local = @Local(type = Player.class))
     @Expression("player != null")
-    @ModifyExpressionValue(method = "getTooltipLines", at = @At("MIXINEXTRAS:EXPRESSION"))
+    @ModifyExpressionValue(method = "addDetailsToTooltip", at = @At("MIXINEXTRAS:EXPRESSION"))
     private boolean ApexCore$appendTooltipWarningBefore(
             boolean original,
-            @Local Consumer<Component> consumer,
+            @Local(argsOnly = true) Consumer<Component> consumer,
             @Local(argsOnly = true)  Item.TooltipContext context,
             @Local(argsOnly = true) @Nullable Player player,
             @Local(argsOnly = true) TooltipFlag flag
@@ -103,31 +106,27 @@ public class ItemStackMixin {
         return original;
     }
 
-    @Inject(
+    @WrapOperation(
             method = "getTooltipLines",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/neoforged/neoforge/event/EventHooks;onItemTooltip(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;Lnet/minecraft/world/item/Item$TooltipContext;)Lnet/neoforged/neoforge/event/entity/player/ItemTooltipEvent;",
-                    shift = At.Shift.BEFORE
+                    target = "Lnet/neoforged/neoforge/event/EventHooks;onItemTooltip(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;Lnet/minecraft/world/item/Item$TooltipContext;)Lnet/neoforged/neoforge/event/entity/player/ItemTooltipEvent;"
             )
     )
-    private void ApexCore$appendTooltipFooterBefore(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local Consumer<Component> consumer) {
-        var self = (ItemStack) (Object) this;
-        TooltipMutationHandler.mutate(TooltipPosition.WARNING, TooltipOrder.AFTER).accept(self, context, consumer, player, flag);
-        TooltipMutationHandler.mutate(TooltipPosition.FOOTER, TooltipOrder.BEFORE).accept(self, context, consumer, player, flag);
-        TooltipMutationHandler.mutate(TooltipPosition.FOOTER, TooltipOrder.AFTER).accept(self, context, consumer, player, flag);
-        TooltipMutationHandler.mutate(TooltipPosition.MODDED, TooltipOrder.BEFORE).accept(self, context, consumer, player, flag);
-    }
-
-    @Inject(
-            method = "getTooltipLines",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/neoforged/neoforge/event/EventHooks;onItemTooltip(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;Lnet/minecraft/world/item/Item$TooltipContext;)Lnet/neoforged/neoforge/event/entity/player/ItemTooltipEvent;",
-                    shift = At.Shift.AFTER
-            )
-    )
-    private void ApexCore$appendTooltipFooterAfter(Item.TooltipContext context, @Nullable Player player, TooltipFlag flag, CallbackInfoReturnable<List<Component>> cir, @Local Consumer<Component> consumer) {
-        TooltipMutationHandler.mutate(TooltipPosition.MODDED, TooltipOrder.AFTER).accept((ItemStack) (Object) this, context, consumer, player, flag);
+    private ItemTooltipEvent ApexCore$appendTooltipFooterBefore(
+            ItemStack stack,
+            @Nullable Player player,
+            List<Component> lines,
+            TooltipFlag flag,
+            Item.TooltipContext context,
+            Operation<ItemTooltipEvent> original
+    ) {
+        TooltipMutationHandler.mutate(TooltipPosition.WARNING, TooltipOrder.AFTER).accept(stack, context, lines::add, player, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.FOOTER, TooltipOrder.BEFORE).accept(stack, context, lines::add, player, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.FOOTER, TooltipOrder.AFTER).accept(stack, context, lines::add, player, flag);
+        TooltipMutationHandler.mutate(TooltipPosition.MODDED, TooltipOrder.BEFORE).accept(stack, context, lines::add, player, flag);
+        var event = original.call(stack, player, lines, flag, context);
+        TooltipMutationHandler.mutate(TooltipPosition.MODDED, TooltipOrder.AFTER).accept(stack, context, lines::add, player, flag);
+        return event;
     }
 }
