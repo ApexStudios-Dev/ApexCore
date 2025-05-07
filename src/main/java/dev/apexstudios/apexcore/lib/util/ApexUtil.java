9fe0639177fb785528e7e677e0c4914a8ff12ef1
Apex <robsonlawson3@gmail.com>
2025-05-07 08:25:49 +0100

    Remove unused utilities

--- src/main/java/dev/apexstudios/apexcore/lib/util/ApexUtil.java
+++ src/main/java/dev/apexstudios/apexcore/lib/util/ApexUtil.java
@@ -3,7 +3,6 @@ package dev.apexstudios.apexcore.lib.util;
 import com.google.common.base.Predicates;
 import dev.apexstudios.apexcore.mixin.LootTableAccessor;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
@@ -13,15 +12,10 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.CollisionGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelHeightAccessor;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -30,98 +24,12 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.CollisionContext;
 import net.neoforged.neoforge.items.IItemHandler;
 import net.neoforged.neoforge.items.IItemHandlerModifiable;
 import net.neoforged.neoforge.registries.GameData;
 import org.jetbrains.annotations.Nullable;
 
 public interface ApexUtil {
-    static boolean isInWorldBounds(LevelHeightAccessor level, BlockPos pos) {
-        return !level.isOutsideBuildHeight(pos) && isInWorldBoundsHorizontal(pos);
-    }
-
-    static boolean isInWorldBoundsHorizontal(BlockPos pos) {
-        return pos.getX() >= -30000000 && pos.getZ() >= -30000000 && pos.getX() < 30000000 && pos.getZ() < 30000000;
-    }
-
-    static <TLevel extends LevelHeightAccessor & CollisionGetter> boolean isInBounds(TLevel level, BlockPos pos) {
-        if(!isInWorldBounds(level, pos))
-            return false;
-        if(!level.getWorldBorder().isWithinBounds(pos))
-            return false;
-
-        return true;
-    }
-
-    static boolean canPlace(LevelReader level, BlockPos pos, BlockState blockState, @Nullable LivingEntity placer) {
-        if(!blockState.isAir() && !level.getBlockState(pos).canBeReplaced())
-            return false;
-
-        return mayPlace(level, pos, blockState, placer);
-    }
-
-    static boolean canPlace(BlockPlaceContext context, BlockState blockState) {
-        var level = context.getLevel();
-        var pos = context.getClickedPos();
-
-        if(!blockState.isAir() && !level.getBlockState(pos).canBeReplaced(context))
-            return false;
-
-        return mayPlace(level, pos, blockState, context.getPlayer());
-    }
-
-    private static boolean mayPlace(LevelReader level, BlockPos pos, BlockState blockState, @Nullable LivingEntity placer) {
-        if(!blockState.canSurvive(level, pos))
-            return false;
-
-        var collisionContext = placer == null ? CollisionContext.empty() : CollisionContext.of(placer);
-
-        if(!level.isUnobstructed(blockState, pos, collisionContext))
-            return false;
-
-        if(placer instanceof Player player) {
-            if(!player.mayBuild())
-                return false;
-            if(level instanceof Level lvl && !lvl.mayInteract(player, pos))
-                return false;
-        }
-
-        return true;
-    }
-
-    static <TValue> void addAll(Collection<? super TValue> collection, Iterable<? extends TValue> values) {
-        for(var value : values) {
-            collection.add(value);
-        }
-    }
-
-    @SafeVarargs
-    static <TValue> void removeAll(Collection<? super TValue> collection, TValue... values) {
-        for(var value : values) {
-            collection.remove(value);
-        }
-    }
-
-    static <TValue> void removeAll(Collection<? super TValue> collection, Iterable<? extends TValue> values) {
-        for(var value : values) {
-            collection.remove(value);
-        }
-    }
-
-    static BlockState withPropertiesOf(BlockState source, BlockState target) {
-        for(var property : target.getProperties()) {
-            if(source.hasProperty(property))
-                source = Block.copyProperty(source, target, property);
-        }
-
-        return source;
-    }
-
-    static <T> Class<T> asClass(Class<? super T> clazz) {
-        return (Class<T>) clazz;
-    }
-
     // copy of RandomizableContainer.unpackLootTable for common usages
     static void unpackLootTable(Level level, BlockPos pos, @Nullable Player player, @Nullable ResourceKey<LootTable> lootTableId, BiConsumer<LootTable, LootParams> filler) {
         if(lootTableId == null || !(level instanceof ServerLevel sLevel))
