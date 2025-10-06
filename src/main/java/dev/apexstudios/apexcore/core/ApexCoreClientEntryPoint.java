c9b8a2323c1b2c381057e58cc4c4c627e21faebc
Apex <robsonlawson3@gmail.com>
2025-10-06 02:46:15 +0100

    Extract multi block breaking positions

--- src/main/java/dev/apexstudios/apexcore/core/ApexCoreClientEntryPoint.java
+++ src/main/java/dev/apexstudios/apexcore/core/ApexCoreClientEntryPoint.java
@@ -1,11 +1,18 @@
 package dev.apexstudios.apexcore.core;
 
+import com.google.common.collect.Lists;
 import dev.apexstudios.apexcore.core.client.DyeColorItemTintSource;
+import dev.apexstudios.apexcore.lib.multiblock.MultiBlock;
 import dev.apexstudios.apexcore.lib.util.ApexRenderTypes;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.state.BlockBreakingRenderState;
+import net.minecraft.client.renderer.state.LevelRenderState;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.ExtractLevelRenderStateEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(value = ApexCore.ID, dist = Dist.CLIENT)
 public final class ApexCoreClientEntryPoint {
@@ -13,5 +20,27 @@ public final class ApexCoreClientEntryPoint {
         ApexRenderTypes.register(modBus);
 
         modBus.addListener(RegisterColorHandlersEvent.ItemTintSources.class, event -> event.register(ApexCore.identifier("dye_color"), DyeColorItemTintSource.MAP_CODEC));
+
+        NeoForge.EVENT_BUS.addListener(ExtractLevelRenderStateEvent.class, event -> {
+            var renderState = event.getRenderState();
+            var level = event.getLevel();
+
+            extractMultiBlockBreakingProgress(level, renderState);
+        });
+    }
+
+    private void extractMultiBlockBreakingProgress(ClientLevel level, LevelRenderState renderState) {
+        var additionalBreakingStates = Lists.<BlockBreakingRenderState>newArrayList();
+
+        for(var breakingState : renderState.blockBreakingRenderStates) {
+            var index = MultiBlock.getIndex(breakingState.blockState);
+
+            MultiBlock.forEachPos(breakingState.blockPos, breakingState.blockState, (otherPos, otherBlockState) -> {
+                if(MultiBlock.getIndex(otherBlockState) != index)
+                    additionalBreakingStates.add(new BlockBreakingRenderState(level, otherPos, breakingState.progress));
+            });
+        }
+
+        renderState.blockBreakingRenderStates.addAll(additionalBreakingStates);
     }
 }
