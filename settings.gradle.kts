f427482b9fbeb61ed4bc5d42ab52537559a2628a
Apex <robsonlawson3@gmail.com>
2025-10-26 13:45:40 +0000

    Use published registree/placement visualizer versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("registree", "dev.apexstudios", "registree").version("21.10.2-beta-pr-1")
-        library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.10.3-beta-pr-1")
+        library("registree", "dev.apexstudios", "registree").version("21.10.2")
+        library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.10.1")
     }
 }
 
