33b5accaedfe95dd1e058791176cb280e8998f52
Apex <29412632+ApexModder@users.noreply.github.com>
2025-12-10 13:31:12 +0000

    Greatly improve the dyeable system (#65)

--- src/main/java/dev/apexstudios/apexcore/core/client/DyeColorItemTintSource.java
+++ src/main/java/dev/apexstudios/apexcore/core/client/DyeColorItemTintSource.java
@@ -1,23 +1,31 @@
 package dev.apexstudios.apexcore.core.client;
 
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
+import dev.apexstudios.apexcore.lib.block.Dyeable;
 import net.minecraft.client.color.item.ItemTintSource;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.util.CommonColors;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
 
-public record DyeColorItemTintSource(DyeColor defaultColor) implements ItemTintSource {
-    public static final MapCodec<DyeColorItemTintSource> MAP_CODEC = RecordCodecBuilder.mapCodec(builder -> builder.group(
-            DyeColor.CODEC.fieldOf("default").forGetter(DyeColorItemTintSource::defaultColor)
-    ).apply(builder, DyeColorItemTintSource::new));
+public final class DyeColorItemTintSource implements ItemTintSource {
+    public static final MapCodec<DyeColorItemTintSource> MAP_CODEC = MapCodec.unit(DyeColorItemTintSource::new);
 
     @Override
     public int calculate(ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity entity) {
-        return stack.getOrDefault(DataComponents.BASE_COLOR, defaultColor).getTextureDiffuseColor();
+        Dyeable.DyedColor dyedColor;
+
+        if(Block.byItem(stack.getItem()) instanceof Dyeable dyeable) {
+            dyedColor = dyeable.getDyedColor(stack);
+        } else {
+            dyedColor = Dyeable.DyedColor.from(stack.get(DataComponents.BASE_COLOR));
+        }
+
+        var color = dyedColor.getColor();
+        return color == null ? CommonColors.WHITE : color.getTextureDiffuseColor();
     }
 
     @Override
