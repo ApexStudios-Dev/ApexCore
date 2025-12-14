af15369932625376c44e15afb551838576dffffd
Apex <29412632+ApexModder@users.noreply.github.com>
2025-12-14 07:37:22 +0000

    [21.11] Merge Development Changes (#68)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("registree", "dev.apexstudios", "registree").version("21.11.0")
+        library("registree", "dev.apexstudios", "registree").version("21.11.1")
         library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.11.0")
     }
 }
