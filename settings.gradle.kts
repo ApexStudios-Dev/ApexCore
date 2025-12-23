927120a0c581a46de28fdaa6ea6cb298b4b36487
Apex <29412632+ApexModder@users.noreply.github.com>
2025-12-23 10:46:45 +0000

    Bump PlacementVisualizer (#71)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
         library("registree", "dev.apexstudios", "registree").version("21.11.1")
-        library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.11.0")
+        library("placementvisualizer", "dev.apexstudios", "placementvisualizer").version("21.11.2")
     }
 }
 
