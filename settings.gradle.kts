be05bed2e594d6231258774f6caaab85882f9a76
Apex <robsonlawson3@gmail.com>
2026-02-18 21:52:16 +0000

    Bump registree

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
     versionCatalogs.create("libs") {
         version("neoforge", "26.1.0.0-alpha.11+snapshot-7")
 
-        library("registree", "dev.apexstudios", "registree").version("26.1.16-beta-pr-17")
+        library("registree", "dev.apexstudios", "registree").version("26.1.37-beta-pr-19")
     }
 }
 
