e8b5c993b45a21535330180f3e377c23c101454a
Apex <robsonlawson3@gmail.com>
2026-06-16 17:08:28 +0100

    Bump to 26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,18 +17,18 @@ neoForge {
 }
 
 repositories {
-    maven("https://prmaven.neoforged.net/NeoForge/pr3198") {
+    /*maven("https://prmaven.neoforged.net/NeoForge/pr3198") {
         content {
             includeModule("net.neoforged", "neoforge")
             includeModule("net.neoforged", "testframework")
         }
-    }
+    }*/
 
-    maven("https://maven.apexmodder.com/prs/Registree/pr29") {
+    /*maven("https://maven.apexmodder.com/prs/Registree/pr29") {
         content {
             includeModule("dev.apexstudios", "registree")
         }
-    }
+    }*/
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,9 +19,9 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.2.0-alpha.0+pre-4.20260607.184824")
+        version("neoforge", "26.2.0.0-beta")
 
-        library("registree", "dev.apexstudios", "registree").version("26.2.1-beta-pr-29")
+        library("registree", "dev.apexstudios", "registree").version("26.2.0")
     }
 }
 
