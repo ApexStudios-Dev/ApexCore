4310c0e646e0f1123f74dd7aa19369e474396547
Apex <robsonlawson3@gmail.com>
2026-09-19 20:56:41 +0100

    Bump NeoForge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.3.0.12-beta-pr-3492-pr-data-gen-extensions")
+        version("neoforge", "26.3.0.7-beta")
 
         library("registree", "dev.apexstudios", "registree").version("26.3.3-beta-pr-37")
     }
